a014074783855b961a11e6c531fb3a41bf720c86
Nikolay <repyah84@gmail.com>
2023-10-01 11:37:25 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppModuleCss from "./App.module.scss";
+
+const App = () => {
+  return (
+    <div className={AppModuleCss.host}>
+      <h1>HElo</h1>
+    </div>
+  );
+};
+
+export { App };
